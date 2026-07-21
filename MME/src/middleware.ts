89e65bdb4bfed13140cf85d0f295de5a27a42721
Avelin <bassindo00@gmail.com>
2026-07-21 14:00:26 +0700

    Update admin email to admin@mme.com

--- MME/src/middleware.ts
+++ MME/src/middleware.ts
@@ -9,7 +9,7 @@ export default auth(async (req) => {
 
   const isLoggedIn = !!req.auth?.user
   // @ts-ignore
-  const role = req.auth?.user?.role || req.auth?.role || (req.auth?.user?.email === 'admin@breakoutmusic.online' ? 'ADMIN' : 'USER')
+  const role = req.auth?.user?.role || req.auth?.role || (req.auth?.user?.email === 'admin@mme.com' ? 'ADMIN' : 'USER')
   // @ts-ignore
   const status = req.auth?.user?.status || req.auth?.status || 'APPROVED'
 
